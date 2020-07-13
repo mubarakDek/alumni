d7df0d7d0add46057b9fc2104d03f866d5592168
Mubarak Dek <dekmubarak@gmail.com>
2020-07-13 05:42:10 +0300

    folders created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// Components
+import Header from "./components/Header"
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
         <h1>Alumni System</h1>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/main.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
